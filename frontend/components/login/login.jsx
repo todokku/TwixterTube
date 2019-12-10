f4b317b4a3017c9ba8400d4242f3c75ff7865cfc
ashap94 <aaronshapiro94@gmail.com>
2019-12-10 13:12:17 -0800

    Changed font family on login and signup pages as well as even handler to navigate back to home page

--- frontend/components/login/login.jsx
+++ frontend/components/login/login.jsx
@@ -1,92 +1,115 @@
-import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
 
 class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: "",
-            password: ""
-        }
-        this.update = this.update.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleDemoUser = this.handleDemoUser.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+    this.update = this.update.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDemoUser = this.handleDemoUser.bind(this);
+    this.handleHomePage = this.handleHomePage.bind(this);
+  }
 
-    componentDidMount() {
-        this.props.clearErrors();
-    }
+  componentDidMount() {
+    this.props.clearErrors();
+  }
 
-    update(field) {
-        return e => this.setState({
-            [field]: e.target.value
-        })
-    }
+  update(field) {
+    return e =>
+      this.setState({
+        [field]: e.target.value
+      });
+  }
 
-    handleSubmit(e) {
-        e.preventDefault();
-        this.setState({
-            email: this.state.email.trim()
-        });
-        this.props.login(this.state)
-            .then(() => this.props.history.push('/'));
-    }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      email: this.state.email.trim()
+    });
+    this.props.login(this.state).then(() => this.props.history.push("/"));
+  }
 
-    renderErrors () {
-        return (
-            <ul>
-                {this.props.errors.map( (error, idx) => (
-                    <li key={idx} className='errors-text-login'>{error}</li>
-                ))}
-            </ul>
-        )
-    }
+  handleHomePage() {
+    this.props.history.push("/");
+  }
 
-    nextPath(path) {
-        this.props.history.push(path);
-    }
+  renderErrors() {
+    return (
+      <ul>
+        {this.props.errors.map((error, idx) => (
+          <li key={idx} className="errors-text-login">
+            {error}
+          </li>
+        ))}
+      </ul>
+    );
+  }
 
-    handleDemoUser(e) {
-        this.setState({
-            email: 'd116',
-            password: '123456'
-        })
-    }
+  nextPath(path) {
+    this.props.history.push(path);
+  }
 
-    render() {
-        return (
-            <div className='session-form-container'>
-                <form onSubmit={this.handleSubmit} className='session-form'>
-                    <div className="logo">
-                        <h2>TwixterTube</h2>
-                    </div>         
-                    <header className="session-form-header">
-                        <h2>Sign In</h2>
-                        <h4>to continue to TwixterTube</h4>
-                    </header>
-                    <div className='session-input-fields'>
-                        <label>
-                            <input type="text" id="email-input" value={this.state.email} placeholder='Email' onChange={this.update('email')} />
-                        </label>
-                        <label>
-                            <input type="password" id='password-input' value={this.state.password} placeholder='Password' onChange={this.update('password')} />
-                        </label>
-                    </div>
-                    <div className='session-form-buttons'>
-                        <Link to={`/signup`} className='session-form-buttons-Links'>
-                            Create account
-                        </Link>
-                        {/* <button onClick={() => this.nextPath('/signup') }>
+  handleDemoUser(e) {
+    this.setState({
+      email: "d116",
+      password: "123456"
+    });
+  }
+
+  render() {
+    return (
+      <div className="session-form-container">
+        <form onSubmit={this.handleSubmit} className="session-form">
+          <div className="logo" onClick={this.handleHomePage}>
+            <h2>TwixterTube</h2>
+          </div>
+          <header className="session-form-header">
+            <h2>Sign In</h2>
+            <h4>to continue to TwixterTube</h4>
+          </header>
+          <div className="session-input-fields">
+            <label>
+              <input
+                type="text"
+                id="email-input"
+                value={this.state.email}
+                placeholder="Email"
+                onChange={this.update("email")}
+              />
+            </label>
+            <label>
+              <input
+                type="password"
+                id="password-input"
+                value={this.state.password}
+                placeholder="Password"
+                onChange={this.update("password")}
+              />
+            </label>
+          </div>
+          <div className="session-form-buttons">
+            <Link to={`/signup`} className="session-form-buttons-Links">
+              Create account
+            </Link>
+            {/* <button onClick={() => this.nextPath('/signup') }>
                             Create account
                         </button> */}
-                        <button onClick={this.handleSubmit} className='next-button'>Next</button>
-                        <button onClick={this.handleDemoUser} className='next-button'>Demo</button>
-                    </div>
-                </form>
-                {this.renderErrors()}
-            </div>
-        )
-    }
+            <button onClick={this.handleSubmit} className="next-button">
+              Next
+            </button>
+            <button onClick={this.handleDemoUser} className="next-button">
+              Demo
+            </button>
+          </div>
+        </form>
+        {this.renderErrors()}
+      </div>
+    );
+  }
 }
 
 export default withRouter(Login);
--- frontend/components/signup/signup.jsx
+++ frontend/components/signup/signup.jsx
@@ -1,109 +1,129 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Link, withRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Link, withRouter } from "react-router-dom";
 
 class Signup extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: "",
-            email: "",
-            password: ""
-        }
-        this.update = this.update.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      email: "",
+      password: ""
+    };
+    this.update = this.update.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleHomePage = this.handleHomePage.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.clearErrors();
+  }
+
+  update(field) {
+    return e =>
+      this.setState({
+        [field]: e.target.value
+      });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      username: this.state.username.trim(),
+      email: this.state.email.trim()
+    });
+    this.props.createUser(this.state).then(() => this.props.history.push("/"));
+  }
+
+  obtainErrors() {
+    // return (
+    //         this.props.errors.map((error, idx) => (
+    //             <li key={idx}>{error}</li>
+    //         ))
+    // )
+    return this.props.errors;
+  }
+
+  handleHomePage() {
+    this.props.history.push("/");
+  }
+
+  nextPath(path) {
+    this.props.history.push(path);
+  }
+
+  render() {
+    let errors = this.obtainErrors();
+    let userErrors = undefined;
+    let emailErrors = undefined;
+    let passwordErrors = undefined;
+    let i = 0;
+
+    while (i < errors.length) {
+      if (errors[i].includes("Username")) {
+        userErrors = <li className="errors-text">{errors[i]}</li>;
+      } else if (errors[i].includes("Email")) {
+        emailErrors = <li className="errors-text">{errors[i]}</li>;
+      } else if (errors[i].includes("Password")) {
+        passwordErrors = <li className="errors-text">{errors[i]}</li>;
+      }
+      i++;
     }
 
-    componentDidMount() {
-        this.props.clearErrors();
-    }
-
-    update(field) {
-        return e => this.setState({
-            [field]: e.target.value
-        })
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-        this.setState({
-            username: this.state.username.trim(),
-            email: this.state.email.trim()
-        });
-        this.props.createUser(this.state)
-            .then( () => this.props.history.push('/'));
-    }
-
-    obtainErrors() {
-
-        // return (
-        //         this.props.errors.map((error, idx) => (
-        //             <li key={idx}>{error}</li>
-        //         ))
-        // )
-        return (
-            this.props.errors
-        )
-    }
-
-    nextPath(path) {
-        this.props.history.push(path);
-    }
-
-    render() {
-        let errors = this.obtainErrors();
-        let userErrors = undefined;
-        let emailErrors = undefined;
-        let passwordErrors = undefined;
-        let i = 0;
-
-        while (i < errors.length) {
-            if ( errors[i].includes('Username') ) {
-                userErrors = <li className="errors-text">{errors[i]}</li>;
-            } else if (errors[i].includes('Email')) {
-                emailErrors = <li className="errors-text">{errors[i]}</li>;
-            } else if (errors[i].includes('Password')) {
-                passwordErrors = <li className="errors-text" >{errors[i]}</li>;
-            }
-            i++;
-        }
-
-        return (
-            <div className='session-form-container'>
-                <form onSubmit={this.handleSubmit} className='session-form'>
-                <div className="logo">
-                    <h2>TwixterTube</h2>
-                </div>
-                <header className="session-form-header">
-                    <h2>Create your Account</h2>
-                    <h4>to continue to TwixterTube</h4>
-                </header>
-                    <div className='session-input-fields'>
-                        <label>
-                            <input type="text" value={this.state.username} id='username-input' placeholder='Username' onChange={this.update('username')} />
-
-                        </label>
-                        {userErrors}
-                        <label>
-                            <input type="text" value={this.state.email} id='email-input' placeholder='Email' onChange={this.update('email')}/>
-                        </label>
-                        {emailErrors}
-                        <label>
-                            <input type="password" value={this.state.password} id='password-input' placeholder='Password' onChange={this.update('password')} />
-                        </label>
-                        {passwordErrors}
-                    </div>
-                    <div className='session-form-buttons'>
-                        <Link to={`/login`} className='session-form-buttons-Links'>
-                            Sign in instead
-                        </Link>
-                        <button onClick={this.handleSubmit} className='next-button'>Next</button>
-                    </div>
-                </form>
-            </div>
-        )
-    }
+    return (
+      <div className="session-form-container">
+        <form onSubmit={this.handleSubmit} className="session-form">
+          <div className="logo" onClick={this.handleHomePage}>
+            <h2>TwixterTube</h2>
+          </div>
+          <header className="session-form-header">
+            <h2>Create your Account</h2>
+            <h4>to continue to TwixterTube</h4>
+          </header>
+          <div className="session-input-fields">
+            <label>
+              <input
+                type="text"
+                value={this.state.username}
+                id="username-input"
+                placeholder="Username"
+                onChange={this.update("username")}
+              />
+            </label>
+            {userErrors}
+            <label>
+              <input
+                type="text"
+                value={this.state.email}
+                id="email-input"
+                placeholder="Email"
+                onChange={this.update("email")}
+              />
+            </label>
+            {emailErrors}
+            <label>
+              <input
+                type="password"
+                value={this.state.password}
+                id="password-input"
+                placeholder="Password"
+                onChange={this.update("password")}
+              />
+            </label>
+            {passwordErrors}
+          </div>
+          <div className="session-form-buttons">
+            <Link to={`/login`} className="session-form-buttons-Links">
+              Sign in instead
+            </Link>
+            <button onClick={this.handleSubmit} className="next-button">
+              Next
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default withRouter(Signup);
